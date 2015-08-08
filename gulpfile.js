2c8c28e53e2ce0179bd5c8220db9e1fc9d210528
Max Norton <maxnorton.nz@gmail.com>
2015-08-08 12:04:31 +1000

    new serveprod gulp task run

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,14 @@ gulp.task('heroku', function() {
     }));
 });
 
+gulp.task('serveprod', function() {
+  connect.server({
+    root: 'app',
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
+});
+
 
 gulp.task('less', function() {
   gulp.src('app/styles/app.less')
